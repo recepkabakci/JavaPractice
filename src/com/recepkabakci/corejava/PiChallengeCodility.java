5d1c5b09909e277c2d2149d1b90e3495b793b77a
RECEP KABAKCI <recepkabakci90@gmail.com>
2023-05-01 13:13:46 +0200

    update

--- src/com/recepkabakci/corejava/PiChallengeCodility.java
+++ src/com/recepkabakci/corejava/PiChallengeCodility.java
@@ -14,16 +14,7 @@ public class PiChallengeCodility {
 		String P="recep";
 		String Q="akbas";
 		int N= P.toCharArray().length;
-		char[] newsArray = P.concat(Q).toCharArray();
-		String last="";
-		for(int i=0;i<N;i++) {
-			int randomX = random.nextInt(i);
-			last.concat(newsArray[randomX]);		
-		}
-		for(int i=0;i< vowelArray.length;i++) {
-			hash.add(vowelArray[i]);			
-		}
-		System.out.println(hash.size());
+		
 	}
 
 }
